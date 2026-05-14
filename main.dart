ff300824da7f98d4641c0e468156d6caf8c0f94f
hansaka457 <hansakadissanayaka916@gmail.com>
2026-05-14 09:24:18 +0530

    Initialize Flutter app with basic structure

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'If Wishes Could Kill',
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: Center(child: Text('Site eka live welu! 🔥', style: TextStyle(fontSize: 24))),
+      ),
+    );
+  }
+}
